d8bd9856b3002ab5a40a4d66e695f502f2cc3ab1
waterzhu <zhumingyi1994@gmail.com>
2020-10-09 22:27:26 +0800

    判断环形链表

---
+++ 20201009环形链表/circlelinktable.cc
@@ -0,0 +1,26 @@
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return false;
+        ListNode* first = head;
+        ListNode* second = head->next;
+        while (first && second)
+        {
+            if (first == second)
+                return true;
+            first = first->next;
+            if (second->next == nullptr)
+                return false;
+            second = second->next->next;
+        }
+        return false;
+    }
+};
